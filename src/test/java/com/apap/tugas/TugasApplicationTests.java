cad2cf1f2c8ddf5ec96486fa3f479a6c25807101
farid wajdi <faridwajdi05@gmail.com>
2018-11-09 13:18:54 +0700

    initial commit

---
+++ src/test/java/com/apap/tugas/TugasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apap.tugas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TugasApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
