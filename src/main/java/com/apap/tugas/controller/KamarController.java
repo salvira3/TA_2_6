7d12c21797dea10d2ec8ef48f7c2a1dc8aaa6dbc
Ismail Al Ghani <ismailalghani@gmail.com>
2018-11-22 19:10:02 +0700

    fitur 13 done

--- src/main/java/com/apap/tugas/controller/KamarController.java
+++ src/main/java/com/apap/tugas/controller/KamarController.java
@@ -1,9 +1,14 @@
 package com.apap.tugas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.apap.tugas.model.KamarModel;
+import com.apap.tugas.model.PaviliunModel;
 import com.apap.tugas.service.KamarService;
 import com.apap.tugas.service.PaviliunService;
 
@@ -15,4 +20,18 @@ public class KamarController {
 	
 	@Autowired
 	private PaviliunService paviliunService;
+	
+	@RequestMapping(value = "")
+	private String listKamar(Model model) {
+		List<KamarModel> listKamar = kamarService.getAll();
+		model.addAttribute("listKamar" , listKamar);
+		return "list-kamar";
+	}
+	
+	@RequestMapping(value = "/insert")
+	private String addKamar(Model model) {
+		List<PaviliunModel> listPaviliun = paviliunService.getAll();
+		model.addAttribute("listPaviliun" , listPaviliun);
+		return "add-kamar";
+	}
 }
