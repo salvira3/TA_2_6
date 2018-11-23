b8eb50ba6d82ca77d5c5f6ddb10b27e775687987
wildaparasti <wildasti98@gmail.com>
2018-11-23 16:30:35 +0700

    no 7 done

--- src/main/java/com/apap/tugas/controller/PemeriksaanController.java
+++ src/main/java/com/apap/tugas/controller/PemeriksaanController.java
@@ -2,6 +2,7 @@ package com.apap.tugas.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 
 import com.apap.tugas.model.DokterModel;
 import com.apap.tugas.model.PasienModel;
+import com.apap.tugas.model.PemeriksaanModel;
+import com.apap.tugas.service.PemeriksaanService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -18,11 +21,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class PemeriksaanController {
 	RestTemplate restTemplate = new RestTemplate();
 	
-/**	@RequestMapping(value = "/penanganan/{idPasien}", method = RequestMethod.GET)
-	private String daftarRequest(@PathVariable (value ="idPasien") long idPasien , Model model) throws IOException {
-		
+	@Autowired
+	PemeriksaanService pemeriksaanService; 
 	
-	}**/	
+	@RequestMapping(value = "/penanganan/{idPasien}", method = RequestMethod.GET)
+	private String daftarRequest(@PathVariable (value ="idPasien") long idPasien , Model model) throws IOException {
+		PemeriksaanModel pemeriksaan = pemeriksaanService.getPemeriksaanDetailByIdPasien(idPasien);
+		PasienModel pasien = getPasienDataFromApi(idPasien);
+		DokterModel dokter = getDokterDataFromApi(pemeriksaan.getIdDokter());
+		
+		model.addAttribute("dokter" , dokter);
+		model.addAttribute("pasien" , pasien);
+		model.addAttribute("pemeriksaan", pemeriksaan);
+		return "lihat-pemeriksaan";
+	
+	}	
 	private PasienModel getPasienDataFromApi(long id) throws IOException {
 		String path = "http://si-appointment.herokuapp.com/api/getPasien/"+ id;
 		String responsenya = restTemplate.getForEntity(path, String.class).getBody();
---
+++ src/main/java/com/apap/tugas/service/PemeriksaanService.java
@@ -0,0 +1,8 @@
+package com.apap.tugas.service;
+
+import com.apap.tugas.model.PemeriksaanModel;
+
+public interface PemeriksaanService {
+	PemeriksaanModel getPemeriksaanDetailByIdPasien(long id);
+	
+}
