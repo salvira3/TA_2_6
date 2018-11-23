f768d859f3af64e016823e6f6da068ebdda9fb0c
farid wajdi <faridwajdi05@gmail.com>
2018-11-23 20:38:44 +0700

    print all doctor from SIAPPOINTMENT on jadwal-jaga

--- src/main/java/com/apap/tugas/controller/JadwalJagaController.java
+++ src/main/java/com/apap/tugas/controller/JadwalJagaController.java
@@ -1,13 +1,17 @@
 package com.apap.tugas.controller;
 
 import com.apap.tugas.configuration.Config;
+import com.apap.tugas.model.DokterModel;
 import com.apap.tugas.rest.BaseResponse;
 import com.apap.tugas.rest.Setting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 public class JadwalJagaController {
@@ -19,10 +23,11 @@ public class JadwalJagaController {
     private Config config;
 
     @RequestMapping(value = "/jadwal-jaga",method = RequestMethod.GET)
-    private String jadwalJaga() {
+    private String jadwalJaga(Model model) {
         String path = Setting.getAllDokterUrl;
-        BaseResponse response = restTemplate.getForObject(path, BaseResponse.class);
-        System.out.println(response.getMessage() + response.getStatus() + response.getResult());
+        BaseResponse<List<DokterModel>> response = restTemplate.getForObject(path, BaseResponse.class);
+        List<DokterModel> listOfDokter = response.getResult();
+        model.addAttribute("listOfDokter",listOfDokter);
         return "jadwal-jaga";
     }
 }
