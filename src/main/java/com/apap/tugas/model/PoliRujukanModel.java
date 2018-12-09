f1be74c678707362552613e53a0b1a835866aabe
Cut Syifa Salvira <salviraa3@gmail.com>
2018-12-09 21:34:01 +0700

    poli rujukan

--- src/main/java/com/apap/tugas/model/PoliRujukanModel.java
+++ src/main/java/com/apap/tugas/model/PoliRujukanModel.java
@@ -4,6 +4,15 @@ import java.io.Serializable;
 
 public class PoliRujukanModel implements Serializable{
 	private int id;
+	private String nama;
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
 
 	public int getId() {
 		return id;
