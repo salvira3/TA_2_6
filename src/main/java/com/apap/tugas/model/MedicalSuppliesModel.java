f762cc6dd72265af23b47189cfe060b9f93df9ce
farid wajdi <faridwajdi05@gmail.com>
2018-12-10 14:52:47 +0700

    update Request Obat Model, changed into SIFarmasi's new format

---
+++ src/main/java/com/apap/tugas/model/MedicalSuppliesModel.java
@@ -0,0 +1,13 @@
+package com.apap.tugas.model;
+
+public class MedicalSuppliesModel {
+    ObatModel medicalSupplies;
+
+    public ObatModel getMedicalSupplies() {
+        return medicalSupplies;
+    }
+
+    public void setMedicalSupplies(ObatModel medicalSupplies) {
+        this.medicalSupplies = medicalSupplies;
+    }
+}
--- src/main/java/com/apap/tugas/rest/Setting.java
+++ src/main/java/com/apap/tugas/rest/Setting.java
@@ -1,6 +1,6 @@
 package com.apap.tugas.rest;
 
 public class Setting {
-    final public static String requestObatUrl = "https://275223c0-c975-4a20-97bd-074520c69e15.mock.pstmn.io";
+    final public static String requestObatUrl = "http://si-farmasi-ocir.herokuapp.com/api/medical-supplies/permintaan";
     final public static String getAllDokterUrl = "http://si-appointment.herokuapp.com/api/2/getAllDokter";
 }
